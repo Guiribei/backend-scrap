fbefc3fed7cf034ee7b0d592d7d93681beaed34a
Guiribei <guilherme_f_ribeiro@hotmail.com>
2025-04-20 11:49:32 -0300

    simplest server

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handleHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello world!")
+}
+
+func main() {
+	http.HandleFunc("/hello", handleHello)
+
+	fmt.Println("Server running at localhost:8000")
+	http.ListenAndServe(":8080", nil)
+}
